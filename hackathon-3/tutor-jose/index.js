e70cb75e3c0ca652080f65c9ea49b9aea61335de
jamz <jamzdeveloper@gmail.com>
2025-06-14 10:59:52 -0500

    ejercicio 1-5

---
+++ hackathon-3/tutor-jose/index.js
@@ -0,0 +1,40 @@
+console.log("hola mundo");
+const dato = prompt("ingrese un numero")
+
+
+if(dato>99 & dato<1000){
+    console.log("El numero tiene 3 digitos")
+}else{
+    console.log("El numero  no tiene 3 digitos")
+}
+
+// let --> cambia su valor --> espacios
+//var --> cambia su valor
+//const --> "cambia su valor" -->espacios
+
+
+// {
+// //   var valor = 5;
+//   {
+//     {
+//       {
+//         const  valor =5;
+//         console.log(valor);
+//         {
+//           console.log(valor);
+//         }
+//       }
+//       const valor=57;
+//       console.log(valor)
+//     }
+//   }
+//   {
+//     {
+
+//         const valor = 89;
+//       {
+//         console.log(valor);
+//       }
+//     }
+//   }
+// }
